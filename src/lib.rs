268297ae5b2c6a50a76f1088e930633e24debfa7
Paulo Cabral Sanz <paulosanz@poli.ufrj.br>
2018-07-29 02:05:05 -0300

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,346 @@
+#[macro_use]
+extern crate log;
+
+use std::{cell::UnsafeCell,
+          fmt::{self, Debug, Formatter},
+          sync::{atomic::{AtomicBool, AtomicUsize, Ordering, ATOMIC_BOOL_INIT, ATOMIC_USIZE_INIT},
+                 Arc,
+                 Weak}};
+
+macro_rules! crit {
+    ($($x: expr),*) => {{
+        error!("CRITICAL ERROR");
+        error!($($x),*);
+        debug_assert!(false, "This should never happened but it did, something is broken and should be fixed");
+    }}
+}
+
+struct VoluntaryServitude<T> {
+    pub cell: UnsafeCell<T>,
+}
+
+impl<T: Debug> Debug for VoluntaryServitude<T> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(
+            f,
+            "VoluntaryServitude {{ cell: UnsafeCell {{ {:?} }} }}",
+            unsafe { &*self.cell.get() }
+        )
+    }
+}
+
+impl<T: Debug> VoluntaryServitude<T> {
+    fn new(value: T) -> VoluntaryServitude<T> {
+        trace!("New VoluntaryServitude based on {:?}", value);
+        VoluntaryServitude {
+            cell: UnsafeCell::new(value),
+        }
+    }
+}
+
+unsafe impl<T> Sync for VoluntaryServitude<T> {}
+
+type ArcNode<T> = Arc<VoluntaryServitude<Node<T>>>;
+type WeakNode<T> = Weak<VoluntaryServitude<Node<T>>>;
+type WrappedNode<T> = VoluntaryServitude<Option<ArcNode<T>>>;
+type WrappedWeak<T> = VoluntaryServitude<Option<WeakNode<T>>>;
+
+#[derive(Debug)]
+struct Node<T> {
+    value: T,
+    next: WrappedNode<T>,
+}
+
+impl<T: Debug> Node<T> {
+    fn arc_node(value: T) -> ArcNode<T> {
+        trace!("New ArcNode Based on {:?}", value);
+        Arc::new(VoluntaryServitude::new(Node {
+            value,
+            next: VoluntaryServitude::new(None),
+        }))
+    }
+}
+
+#[derive(Debug)]
+pub struct VSReadIter<'a, T: 'a> {
+    current: Option<ArcNode<T>>,
+    current_index: usize,
+    size: usize,
+    data: Option<&'a T>,
+}
+
+impl<'a, T: 'a + Debug> Iterator for VSReadIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        trace!("Next element in {:?}", self);
+        if self.current_index == self.size {
+            trace!("No more elements in VSReadIter");
+            self.data = None;
+        } else if self.current_index == 0 && self.current.is_some() {
+            trace!("First VSReadIter element");
+            if let Some(ref current) = self.current {
+                self.data = unsafe { Some(&(*current.cell.get()).value) };
+                self.current_index += 1;
+            } else {
+                crit!("Expected first value but found none: {:?}", self);
+                self.data = None;
+            }
+        } else if self.current.is_some() {
+            trace!("Current is Some");
+            self.current = if let Some(ref current) = self.current {
+                let curr = current.cell.get();
+                let node = match unsafe { &(*(*curr).next.cell.get()) } {
+                    Some(ref next) => {
+                        trace!("Found next node: {:?}", next);
+                        self.current_index += 1;
+                        Arc::clone(next)
+                    }
+                    None => {
+                        crit!("Expected node, but found None");
+                        self.size = self.current_index;
+                        self.data = None;
+                        return self.data;
+                    }
+                };
+
+                unsafe {
+                    self.data = Some(&(*node.cell.get()).value);
+                }
+
+                Some(node)
+            } else {
+                crit!("Current is None when it shouldn't: {:?}", self);
+                None
+            }
+        } else {
+            crit!("self.current is None but it shouldn't: {:?}", self);
+            self.size = self.current_index;
+            self.data = None;
+        }
+
+        trace!("Element: {:?}", self.data);
+        self.data
+    }
+}
+
+pub struct VSRead<T> {
+    writing: AtomicBool,
+    size: AtomicUsize,
+    last_element: WrappedWeak<T>,
+    node: WrappedNode<T>,
+}
+
+impl<T: Debug> Debug for VSRead<T> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let last_element = if let Some(ref weak_node) = unsafe { (&*self.last_element.cell.get()) }
+        {
+            Some(
+                (*weak_node)
+                    .upgrade()
+                    .expect("Failed to upgrade last_element's Weak"),
+            )
+        } else {
+            None
+        };
+        write!(
+            f,
+            "VSRead {{ writing: {:?}, size: {:?}, last_element: {:?}, node: {:?}",
+            self.writing, self.size, last_element, self.node
+        )
+    }
+}
+
+impl<T: Debug> Default for VSRead<T> {
+    fn default() -> Self {
+        VSRead {
+            writing: ATOMIC_BOOL_INIT,
+            size: ATOMIC_USIZE_INIT,
+            last_element: VoluntaryServitude::new(None),
+            node: VoluntaryServitude::new(None),
+        }
+    }
+}
+
+impl<T: Debug> VSRead<T> {
+    pub fn iter<'a>(&self) -> VSReadIter<'a, T> {
+        trace!("Converting VSRead to VSReadIter: {:?}", self);
+        if let Some(ref node) = unsafe { &*self.node.cell.get() } {
+            trace!("VSReadIter start node: {:?}", node);
+            VSReadIter {
+                current: Some(Arc::clone(node)),
+                current_index: 0,
+                size: self.size.load(Ordering::Relaxed),
+                data: None,
+            }
+        } else {
+            VSReadIter {
+                current: None,
+                current_index: 0,
+                size: 0,
+                data: None,
+            }
+        }
+    }
+
+    pub fn append(&self, value: T) {
+        trace!(
+            "Append element to VSRead (size: {}): {:?}",
+            self.size.load(Ordering::Relaxed),
+            value
+        );
+        trace!("Busy blocking waiting for writing lock");
+        while !self.writing.swap(true, Ordering::Relaxed) {}
+        trace!("Holding lock");
+        let last_element = self.last_element.cell.get();
+        let element = if let Some(ref weak_node) = unsafe { &*last_element } {
+            if let Some(node) = weak_node.upgrade() {
+                trace!("Upgraded weak");
+                let raw_node = node.cell.get();
+                let new_node = Node::arc_node(value);
+                let weak = Arc::downgrade(&new_node);
+                unsafe {
+                    (*raw_node).next = VoluntaryServitude::new(Some(new_node));
+                }
+                trace!("Inserted new node after last (unsafe)");
+                Some(weak)
+            } else {
+                crit!("Weak was unable to upgrade, but it should: {:?}", self);
+                let mut size = 0;
+                let mut last = unsafe { &*self.node.cell.get() };
+                loop {
+                    last = if let Some(ref last) = last {
+                        let next_last = unsafe { &*(*last.cell.get()).next.cell.get() };
+                        if next_last.is_some() {
+                            size += 1;
+                        } else {
+                            break;
+                        }
+                        next_last
+                    } else {
+                        last
+                    }
+                }
+
+                if let Some(last) = last {
+                    warn!(
+                        "Obtained last_element again O({}): {:?}",
+                        self.size.load(Ordering::Relaxed),
+                        last
+                    );
+                    unsafe {
+                        *last_element = Some(Arc::downgrade(&last));
+                    }
+                    self.size.store(size, Ordering::Relaxed);
+                    warn!("Calling itself again to be properly ran");
+                    info!("Releasing lock to return early");
+                    self.writing.store(false, Ordering::Relaxed);
+                    return self.append(value);
+                } else {
+                    crit!("No element in list");
+                    self.size.store(0, Ordering::Relaxed);
+                    unsafe {
+                        *self.node.cell.get() = None;
+                        *last_element = None;
+                    }
+                    warn!("Calling itself again to be properly ran");
+                    info!("Releasing lock to return early");
+                    self.writing.store(false, Ordering::Relaxed);
+                    return self.append(value);
+                }
+            }
+        } else {
+            trace!("First element to be inserted");
+            let node = Node::arc_node(value);
+            let weak = Arc::downgrade(&node);
+            unsafe {
+                *self.node.cell.get() = Some(node);
+            }
+            Some(weak)
+        };
+
+        unsafe {
+            *last_element = element;
+        }
+
+        self.size.fetch_add(1, Ordering::Relaxed);
+        trace!("Increased size to: {}", self.size.load(Ordering::Relaxed));
+        trace!("Releasing lock: {:?}", self);
+        self.writing.store(false, Ordering::Relaxed);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate env_logger;
+
+    use super::*;
+    use std::{cmp::max,
+              env::set_var,
+              sync::{Once, ONCE_INIT},
+              thread::spawn};
+
+    static STARTED: Once = ONCE_INIT;
+
+    fn setup() {
+        STARTED.call_once(|| {
+            set_var("RUST_LOG", "trace");
+
+            env_logger::Builder::from_default_env()
+                .default_format_module_path(false)
+                .default_format_timestamp(false)
+                .init();
+        })
+    }
+
+    #[test]
+    fn single_thread() {
+        setup();
+        let list = VSRead::default();
+        for i in 0..20 {
+            list.append(i);
+        }
+
+        for (i, el) in list.iter().enumerate() {
+            assert_eq!(&i, el);
+        }
+    }
+
+    #[test]
+    fn single_in_single_iter() {
+        setup();
+        let count = 20;
+        let list = Arc::new(VSRead::default());
+
+        let list_clone = Arc::clone(&list);
+        let _ = spawn(move || {
+            for i in 0..count {
+                list_clone.append(i + 1)
+            }
+        });
+
+        let mut total_max = 0;
+        let mut last_len = 0;
+        for _ in 0..count {
+            let mut inner_max = 0;
+            let mut len = 0;
+            for (i, num) in list.iter().enumerate() {
+                assert_eq!(i + 1, *num);
+                inner_max = max(i + 1, inner_max);
+                len = max(i, len);
+            }
+            assert!(
+                inner_max > total_max || inner_max == count || len == last_len,
+                "{} > {} || {} == {} || {} == {}",
+                inner_max,
+                total_max,
+                inner_max,
+                count,
+                len,
+                last_len
+            );
+            last_len = len;
+            total_max = inner_max
+        }
+    }
+}
