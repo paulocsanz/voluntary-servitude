1312c524d607826ca9802c88c64c902516c49836
Paulo Cabral Sanz <paulosanz@poli.ufrj.br>
2018-11-01 03:43:44 -0300

    3.0.4

--- src/lib.rs
+++ src/lib.rs
@@ -77,7 +77,7 @@
     while_true
 )]
 #![doc(test(attr(deny(warnings))))]
-#![doc(html_root_url = "https://docs.rs/voluntary_servitude/3.0.3/voluntary-servitude")]
+#![doc(html_root_url = "https://docs.rs/voluntary_servitude/3.0.4/voluntary-servitude")]
 
 #[cfg(feature = "rayon-traits")]
 extern crate rayon as rayon_lib;
