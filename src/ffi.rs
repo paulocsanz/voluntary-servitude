fd310c7cab5d24920f0b69611638d31758e81c81
Paulo Cabral Sanz <paulosanz@poli.ufrj.br>
2018-11-02 16:09:06 -0300

    Update ffi multithread example

--- src/ffi.rs
+++ src/ffi.rs
@@ -86,27 +86,26 @@
 //! ```c
 //! #include<pthread.h>
 //! #include<assert.h>
+//! #include<stdlib.h>
 //! #include<stdio.h>
 //! #include "../include/voluntary_servitude.h"
 //!
-//! const unsigned int num_producers = 4;
 //! const unsigned int num_consumers = 8;
+//! const unsigned int num_producers = 4;
+//! const unsigned int num_threads = 12;
 //!
-//! const unsigned int num_producer_values = 1000;
-//! const unsigned int data[3] = {12, 25, 89};
-//! const size_t last_index = sizeof(data) / sizeof(data[0]) - 1;
+//! const unsigned int num_producer_values = 10000000;
+//! const unsigned int data = 3;
 //!
-//! void * producer();
-//! void * consumer();
+//! void * producer(void *);
+//! void * consumer(void *);
 //!
 //! int main(int argc, char** argv) {
-//!     // You are responsible for making sure 'vs' exists
-//!     // while the producers and consumers do
-//!     vs_t * const vs = vs_new();
-//!     unsigned int current_thread = 0;
+//!     // You are responsible for making sure 'vs' exists while accessed
+//!     vs_t * vs = vs_new();
+//!     uint8_t thread = 0;
 //!     pthread_attr_t attr;
-//!     pthread_t consumers[num_consumers],
-//!               producers[num_producers];
+//!     pthread_t threads[num_threads];
 //!
 //!     if (pthread_attr_init(&attr) != 0) {
 //!         fprintf(stderr, "Failed to initialize pthread arguments.\n");
@@ -114,62 +113,57 @@
 //!     }
 //!
 //!     // Creates producer threads
-//!     for (current_thread = 0; current_thread < num_producers; ++current_thread) {
-//!         if (pthread_create(&producers[current_thread], &attr, &producer, (void *) vs) != 0) {
-//!             fprintf(stderr, "Failed to create producer thread %d.\n", current_thread);
+//!     for (thread = 0; thread < num_producers; ++thread) {
+//!         if (pthread_create(&threads[thread], &attr, &producer, (void *) vs) != 0) {
+//!             fprintf(stderr, "Failed to create producer thread %d.\n", thread);
 //!             exit(-2);
 //!         }
 //!
 //!     }
 //!
 //!     // Creates consumers threads
-//!     for (current_thread = 0; current_thread < num_consumers; ++current_thread) {
-//!         if (pthread_create(&consumers[current_thread], &attr, &consumer, (void *) vs) != 0) {
-//!             fprintf(stderr, "Failed to create consumer thread %d.\n", current_thread);
+//!     for (thread = 0; thread < num_consumers; ++thread) {
+//!         if (pthread_create(&threads[num_producers + thread], &attr, &consumer, (void *) vs) != 0) {
+//!             fprintf(stderr, "Failed to create consumer thread %d.\n", thread);
 //!             exit(-3);
 //!         }
 //!     }
 //!
 //!     // Join all threads, ensuring vs_t* is not used anymore
-//!     for (current_thread = 0; current_thread < num_producers; ++current_thread) {
-//!         pthread_join(producers[current_thread], NULL);
-//!     }
-//!     for (current_thread = 0; current_thread < num_consumers; ++current_thread) {
-//!         pthread_join(consumers[current_thread], NULL);
+//!     for (thread = 0; thread < num_threads; ++thread) {
+//!         pthread_join(threads[thread], NULL);
 //!     }
 //!
 //!     // Never forget to free the memory allocated through the lib
 //!     assert(vs_destroy(vs) == 0);
 //!
-//!     printf("Multi thread example ended without errors\n");
+//!     printf("Multi-thread C example ended without errors\n");
 //!     (void) argc;
 //!     (void) argv;
 //!     return 0;
 //! }
 //!
-//!
-//! void * producer(void * const vs){
+//! void * producer(void * vs){
 //!     unsigned int index;
 //!     for (index = 0; index < num_producer_values; ++index) {
-//!         assert(vs_append(vs, (void *) &data[index % last_index]) == 0);
+//!         assert(vs_append(vs, (void *) &data) == 0);
 //!     }
 //!     return NULL;
 //! }
 //!
-//! void * consumer(void * const vs) {
+//! void * consumer(void * vs) {
 //!     const unsigned int total_values = num_producers * num_producer_values;
-//!     unsigned int values;
+//!     unsigned int values = 0;
 //!
 //!     while (values < total_values) {
-//!         unsigned int sum = (values = 0);
-//!         vs_iter_t * const iter = vs_iter(vs);
-//!         const void * value;
+//!         vs_iter_t * iter = vs_iter(vs);
+//!         void * value;
 //!
+//!         values = 0;
 //!         while ((value = vs_iter_next(iter)) != NULL) {
 //!             ++values;
-//!             sum += *(unsigned int *) value;
 //!         }
-//!         printf("Consumer counts %d elements summing %d.\n", values, sum);
+//!         printf("%d elements\n", values);
 //!
 //!         // Never forget to free the memory allocated through the lib
 //!         assert(vs_iter_destroy(iter) == 0);
